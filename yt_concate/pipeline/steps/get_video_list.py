8f3934cf037d56fb1fe2b74522c4a722e3786cbc
elan ho <shining801014@gmail.com>
2024-06-05 21:15:02 +0800

    Successfully download captions and save video list to file

--- yt_concate/pipeline/steps/get_video_list.py
+++ yt_concate/pipeline/steps/get_video_list.py
@@ -1,17 +1,29 @@
 import urllib.request
 import json
-from yt_concate.settings import API_KEY
+
 from yt_concate.pipeline.steps.step import Step
+from yt_concate.settings import API_KEY
 
 
 class GetVideoList(Step):
-    def process(self, data, inputs):
+    def process(self, data, inputs, utils):
+        print('in get video list')  # 加入了此行，方便同學Debug
+        print(API_KEY)  # 加入了此行，方便同學Debug
+
         channel_id = inputs['channel_id']
+
+        if utils.video_list_file_exists(channel_id):
+            print('Found existing video list file for channel id', channel_id)
+            return self.read_file(utils.get_video_list_filepath(channel_id))
+
         base_video_url = 'https://www.youtube.com/watch?v='
         base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
 
         first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(API_KEY,
+
                                                                                                             channel_id)
+
+        print(first_url)  # 加入了此行，方便同學Debug，遇403 Error者，請把印出的網址貼到瀏覽器中檢視，會寫錯誤原因
 
         video_links = []
         url = first_url
@@ -28,5 +40,19 @@ class GetVideoList(Step):
                 url = first_url + '&pageToken={}'.format(next_page_token)
             except KeyError:
                 break
+
         print(video_links)
+        self.write_to_file(video_links, utils.get_video_list_filepath(channel_id))
+        return video_links
+
+    def write_to_file(self, video_links, filepath):
+        with open(filepath, 'w') as f:
+            for url in video_links:
+                f.write(url + '\n')
+
+    def read_file(self, filepath):
+        video_links = []
+        with open(filepath, 'r') as f:
+            for url in f:
+                video_links.append(url.strip())
         return video_links
